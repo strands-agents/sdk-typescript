29d36f4329fd53b79169ac19bf54cae8c797272a
Patrick Gray <pgrayy@amazon.com>
2026-01-26 15:40:33 -0500

    lint

--- src/models/model.ts
+++ src/models/model.ts
@@ -269,9 +269,7 @@ export abstract class Model<T extends BaseModelConfig = BaseModelConfig> {
           // Store message and stop reason
           if (messageRole) {
             // filter out empty objects
-            contentBlocks = contentBlocks.filter(
-              (block) => !(block instanceof TextBlock && block.text === '')
-            )
+            contentBlocks = contentBlocks.filter((block) => !(block instanceof TextBlock && block.text === ''))
 
             stoppedMessage = new Message({
               role: messageRole,
